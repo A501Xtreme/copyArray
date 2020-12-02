25aa546400ed298b7e8942666fe6276f44306d23
A501Xtreme <alexander.romero-jones@patelhs.org>
2020-12-02 16:34:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    String[] roster1 = {"Trey", "Anthony", "Hayden", "Alex", "Jay", "Alan", "Victor", "Dylan", "Bob", "John"};
+
+    String[] roster2 = new String [10];
+    for (int i = 0; i < 10; i++)
+
+     {
+    roster2[i] = roster1[i];
+    System.out.println(roster2[i]);
+
+    }   
+  }
+}
